e74027cb8495a32c94237b682ad96627945be980
George <eakarpov@yandex.ru>
2018-04-12 00:21:08 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import * as React from 'react';
+import { TaskTemplate } from 'graphlabs.core.template';
+
+const logo = require('./logo.svg');
+
+class App extends TaskTemplate.default {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
